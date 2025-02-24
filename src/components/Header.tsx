e82f19330b2be182ff93626387b6001b5911abc6
tersoo <latifwork14@gmail.com>
2025-02-24 08:04:58 +0300

    Header componenti düzenlendi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsBasket } from "react-icons/bs";
 import { IoIosSearch } from "react-icons/io";
 import CartModal from "./CartModal";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Header() {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
@@ -15,7 +20,6 @@ export default function Header() {
   return (
     <>
       <nav className="h-10 flex items-center px-5 mt-4">
-        {/* Logo ve Menü */}
         <div className="flex-shrink-0 flex items-center space-x-4">
           <div className="flex flex-none items-center justify-center border border-neutral-700 bg-black h-[40px] w-[40px] rounded-xl">
             <svg
@@ -59,7 +63,6 @@ export default function Header() {
           </ul>
         </div>
 
-        {/* Input */}
         <div className="flex-grow max-w-lg ml-30 relative">
           <input
             type="text"
@@ -72,7 +75,6 @@ export default function Header() {
           />
         </div>
 
-        {/* Basket Icon*/}
         <div className="flex-shrink-0 md:w-1/3 flex justify-end">
           <button aria-label="Open cart" onClick={toggleCart}>
             <div className="relative flex group h-11 w-11 items-center justify-center rounded-md border border-neutral-700 text-black transition-colors hover:border-blue-500">
@@ -84,7 +86,7 @@ export default function Header() {
           </button>
         </div>
       </nav>
-      <CartModal isOpen={isCartOpen} onClose={toggleCart} />
+      {isClient && <CartModal isOpen={isCartOpen} onClose={toggleCart} />}
     </>
   );
 }
